054891e7d9de760234f40259d2f7582ec6c18436
pimalek <pi.malek@gmail.com>
2016-12-03 19:45:19 +0100

    pierwsze

---
+++ rozne/src/main/java/takie/Moja.java
@@ -0,0 +1,11 @@
+package takie;
+
+/**
+ * Created by Aga i Piotr on 2016-12-03.
+ */
+public class Moja {
+
+    public static void main(String[] args){
+        System.out.println("moja");
+    }
+}
